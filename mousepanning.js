27305b314b75f8013308b1cf372f25baaf60cd9a
meowcer <bryancosta782@gmail.com>
2026-02-18 14:31:49 -0300

    Add effect options
    Move only when dragging or selecting.

--- mousepanning.js
+++ mousepanning.js
@@ -1,25 +1,33 @@
 var padding = 10;
 var scrollMax = 2000;
+var onlyOnDrag = true;
+var onlyOnSelect = true;
 
 function saveOptions() {
     padding = parseInt(settingsModal.formInputs[0].input.value) ?? 10;
     scrollMax = parseInt(settingsModal.formInputs[1].input.value) ?? 2000;
+    onlyOnDrag = Boolean(settingsModal.cbList[0].cbElm.checked);
+    onlyOnSelect = Boolean(settingsModal.cbList[0].children[0].cbElm.checked);
     w.emit("owot_ext_change", {
         id: "mousepanning",
         settings: {
-            padding, scrollMax
+            padding, scrollMax, onlyOnDrag, onlyOnSelect
         }
     });
 }
 function restoreOptions() {
     settingsModal.formInputs[0].input.value = padding;
     settingsModal.formInputs[1].input.value = scrollMax;
+    settingsModal.cbList[0].cbElm.checked = onlyOnDrag;
+    settingsModal.cbList[0].children[0].cbElm.checked = onlyOnSelect;
 }
 var settingsModal = new Modal();
 settingsModal.createForm();
 settingsModal.setFormTitle("OWoT mouse panning");
 settingsModal.addEntry("Padding", "text", "number");
 settingsModal.addEntry("Scroll max", "text", "number");
+var dragCb = settingsModal.addCheckbox("Only while dragging");
+settingsModal.addCheckbox("Only while selecting", dragCb);
 
 settingsModal.onSubmit(saveOptions);
 settingsModal.onClose(function() {
@@ -31,6 +39,8 @@ w.on("owot_ext_get", function(e) {
     if (e.id != "mousepanning") return;
     padding = e.settings.padding;
     scrollMax = e.settings.scrollMax;
+    onlyOnDrag = e.settings.onlyOnDrag;
+    onlyOnSelect = e.settings.onlyOnSelect;
     restoreOptions();
 });
 w.on("owot_ext_open", function(e) {
@@ -46,13 +56,19 @@ function clearBorders() {
     borderUp = false;
     borderDown = false;
 }
+function checkDragBlocking() {
+	var notDragging = (onlyOnDrag || onlyOnSelect) && !isDragging;
+	var notSelecting = onlyOnSelect && elm.owot.style.cursor != "cell";
+	return notDragging || notSelecting;
+}
 var borderLeft = false;
 var borderRight = false;
 var borderUp = false;
 var borderDown = false;
 var lastFrame = Date.now();
 document.addEventListener("mousemove", function(e) {
-	if (!closest(e.target, elm.owot) && !closest(e.target, elm.announce_container) && !closest(e.target, elm.link_div)) {
+	var notOnCanvas = !closest(e.target, elm.owot) && !closest(e.target, elm.announce_container) && !closest(e.target, elm.link_div);
+	if (notOnCanvas || checkDragBlocking()) {
 		clearBorders();
         return;
     }
@@ -83,6 +99,10 @@ function getOffset(time) {
 }
 function moveBorderMouse() {
 	requestAnimationFrame(moveBorderMouse);
+	if (checkDragBlocking()) {
+		clearBorders();
+		return;
+	}
 	var moveX = 0;
 	var moveY = 0;
 	if (borderLeft) {
